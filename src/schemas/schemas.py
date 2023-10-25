88b24ea60bb627fd51061669f7bc559912f26f01
whoisjose569 <jose.vitor.araujo569@hotmail.com>
2023-10-25 15:38:46 -0300

    Pequenas correções nos tipos de dados e utilização do Response Model

--- src/schemas/schemas.py
+++ src/schemas/schemas.py
@@ -4,16 +4,19 @@ from typing import Optional, List
 
 
 class Usuario(BaseModel):
-    id : Optional[str] = None
+    id : Optional[int] = None
     nome: str
     telefone: str
     #meus_produtos: List[Produto]
     #minhas_vendas: List[Pedido]
     #meus_pedidos: List[Pedido]
     
+    class Config:
+        orm_mode = True
+    
 
 class Produto(BaseModel):
-    id : Optional[str] = None
+    id : Optional[int] = None
     nome: str
     detalhes: str
     preco: float
@@ -22,13 +25,24 @@ class Produto(BaseModel):
     class Config:
         orm_mode = True
 
+class ProdutoSimples(BaseModel):
+    id : Optional[int] = None
+    nome: str
+    preco: float  
+    
+    class Config:
+        orm_mode = True
+
 class Pedido(BaseModel):
-    id : Optional[str] = None
+    id : Optional[int] = None
     # usuario: Usuario
     # Produto: Produto
     quantidade: int
     entrega: bool = True
     endereco: str
     observacoes: Optional[str] = 'Sem observações'
+
+    class Config:
+        orm_mode = True
     
     
--- src/server.py
+++ src/server.py
@@ -1,27 +1,29 @@
-from fastapi import FastAPI, Depends
+from typing import List
+from fastapi import FastAPI, Depends, status
 from src.schemas import schemas
 from src.infra.sqlalchemy.repositorios.produto import RepositorioProduto
 from src.infra.sqlalchemy.repositorios.usuario import RepositorioUsuario
 from src.infra.sqlalchemy.repositorios.pedido import RepositorioPedido
 from sqlalchemy.orm import Session
 from src.infra.sqlalchemy.config.database import get_db, criar_bd
+from src.schemas.schemas import Produto, ProdutoSimples
 
 criar_bd()
 
 app = FastAPI()
 
 
-@app.post('/produtos')
-def criar_produto(produto: schemas.Produto, db: Session = Depends(get_db)):
+@app.post('/produtos', status_code=201, response_model=ProdutoSimples)
+def criar_produto(produto: Produto, db: Session = Depends(get_db)):
     produto_criado = RepositorioProduto(db).criar(produto)
     return produto_criado
 
-@app.get('/produtos')
+@app.get('/produtos', status_code=200, response_model=List[Produto])
 def listar_produtos(db: Session = Depends(get_db)):
     produtos = RepositorioProduto(db).listar()
     return produtos
 
-@app.post('/usuarios')
+@app.post('/usuarios', status_code=201)
 def criar_usuario(usuario: schemas.Usuario, db: Session = Depends(get_db)):
     usuario_criado = RepositorioUsuario(db).criar(usuario)
     return usuario_criado
@@ -31,7 +33,7 @@ def listar_usuarios(db: Session = Depends(get_db)):
     usuarios = RepositorioUsuario(db).listar()
     return usuarios
 
-@app.post('/pedidos')
+@app.post('/pedidos', status_code=201)
 def criar_pedido(pedido: schemas.Pedido, db: Session = Depends(get_db)):
     pedido_criado = RepositorioPedido(db).criar(pedido)
     return pedido_criado
