3feb8c1d8b86a605ee5814c9171a420093c6393d
whoisjose569 <jose.vitor.araujo569@hotmail.com>
2023-10-30 14:01:01 -0300

    Rotas e Metodos criados para as funções atualizar e deletar produtos

--- src/infra/sqlalchemy/repositorios/repositorio_produto.py
+++ src/infra/sqlalchemy/repositorios/repositorio_produto.py
@@ -1,3 +1,4 @@
+from sqlalchemy import update, delete
 from sqlalchemy.orm import Session
 from src.schemas import schemas
 from src.infra.sqlalchemy.models import models
@@ -20,8 +21,13 @@ class RepositorioProduto():
         return produtos
         
     
-    def obter(self):
-        pass
+    def editar(self, produto: schemas.Produto):
+        update_stmt =  update(models.Produto).where(models.Produto.id == produto.id).values(nome=produto.nome, detalhes=produto.detalhes, preco=produto.preco, disponivel=produto.disponivel, usuario_id=produto.usuario_id)
+        
+        self.db.execute(update_stmt)
+        self.db.commit()
     
-    def remover(self):
-        pass
+    def remover(self, id:int):
+        delete_smtm = delete(models.Produto).where(models.Produto.id == id)
+        self.db.execute(delete_smtm)
+        self.db.commit()
--- src/server.py
+++ src/server.py
@@ -1,44 +1,63 @@
 from typing import List
 from fastapi import FastAPI, Depends, status
 from src.schemas import schemas
-from src.infra.sqlalchemy.repositorios.produto import RepositorioProduto
-from src.infra.sqlalchemy.repositorios.usuario import RepositorioUsuario
-from src.infra.sqlalchemy.repositorios.pedido import RepositorioPedido
 from sqlalchemy.orm import Session
 from src.infra.sqlalchemy.config.database import get_db, criar_bd
 from src.schemas.schemas import Produto, ProdutoSimples
+from src.infra.sqlalchemy.repositorios.repositorio_produto import RepositorioProduto
+from src.infra.sqlalchemy.repositorios.repositorio_pedido import RepositorioPedido
+from src.infra.sqlalchemy.repositorios.repositorio_usuario import RepositorioUsuario
 
 #criar_bd()
 
 app = FastAPI()
 
-
+#Criar Produto
 @app.post('/produtos', status_code=201, response_model=ProdutoSimples)
 def criar_produto(produto: Produto, db: Session = Depends(get_db)):
     produto_criado = RepositorioProduto(db).criar(produto)
     return produto_criado
 
+#Listar Produtos
 @app.get('/produtos', status_code=200, response_model=List[Produto])
 def listar_produtos(db: Session = Depends(get_db)):
     produtos = RepositorioProduto(db).listar()
     return produtos
 
+#Atualizar Produto
+@app.put('/produtos')
+def atualizar_produto(produto: schemas.Produto, db: Session = Depends(get_db)):
+    RepositorioProduto(db).editar(produto)
+    return produto
+
+#Remover Produto
+@app.delete('/produtos/{id}')
+def remover_produto(id: int, db: Session = Depends(get_db)):
+    RepositorioProduto(db).remover(id)
+    return
+    
+
+#Criar Usuario
 @app.post('/usuarios', status_code=201)
 def criar_usuario(usuario: schemas.Usuario, db: Session = Depends(get_db)):
     usuario_criado = RepositorioUsuario(db).criar(usuario)
     return usuario_criado
 
+#Listar Usuarios
 @app.get('/usuarios')
 def listar_usuarios(db: Session = Depends(get_db)):
     usuarios = RepositorioUsuario(db).listar()
     return usuarios
 
+#Criar Pedido
 @app.post('/pedidos', status_code=201)
 def criar_pedido(pedido: schemas.Pedido, db: Session = Depends(get_db)):
     pedido_criado = RepositorioPedido(db).criar(pedido)
     return pedido_criado
 
+#Listar Pedidos
 @app.get('/pedidos')
 def listar_pedidos(db: Session = Depends(get_db)):
     pedidos = RepositorioPedido(db).listar()
     return pedidos
+
